c4dcb010f565e5fc9eb098307bfd0235760ba9dd
jeyroik <jeyroik@gmail.com>
2020-06-03 14:57:07 +0300

    fix getting repo

--- src/components/options/TConfigure.php
+++ src/components/options/TConfigure.php
@@ -1,13 +1,13 @@
 <?php
 namespace extas\components\options;
 
+use extas\components\Item;
 use extas\interfaces\options\ICommandOption;
 use extas\interfaces\repositories\IRepository;
 
 /**
  * Trait TConfigure
  *
- * @method IRepository commandOptionRepository()
  * @method addOption(string $name, string $shortcut, int $mode, string $description, $default)
  *
  * @package extas\components\options
@@ -22,10 +22,17 @@ trait TConfigure
      */
     protected function configureWithOptions(string $commandName, array $reserved)
     {
+        $command = new class extends Item {
+            protected function getSubjectForExtension(): string
+            {
+                return 'extas.application';
+            }
+        };
+
         /**
          * @var $options ICommandOption[]
          */
-        $repo = $this->commandOptionRepository();
+        $repo = $command->commandOptionRepository();
         $options = $repo->all([ICommandOption::FIELD__COMMANDS_NAMES => $commandName]);
 
         foreach ($options as $option) {
